6219168efa448606a23904e7c22a3e352bda3820
Veranao <64379851+Veranao@users.noreply.github.com>
2025-05-23 21:00:08 +0000

    Setup login.php page for assignment 1 with username and password

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello Assignment 1</p>'; ?> 
+    <?php echo '<h2>Welcome to the Landing Page of Assignment 1</h2>'; ?> 
+    <?php echo '<p> Click on the button to get to the login page and get started. </p>'; ?>
     <a href="login.php">
       <button id="go" name="go" type="button"> Go To Login Page </button>
     </a>
--- login.php
+++ login.php
@@ -1,1 +1,9 @@
-<h1> Testing Link to Login Page </h1>
+<h1> Welcome to the Login Page for Assignment 1 !</h1>
+
+<form>
+  <label> Username: </label>
+  <input type="text" placeholder="Username..."> </input>
+  <br>
+  <label> Password: </label>
+  <input type="password" placeholder="Password..."> </input>
+</form>
